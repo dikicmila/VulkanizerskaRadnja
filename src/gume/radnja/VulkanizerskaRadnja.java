1542626de0697f0a9a6c852406fb740983c1f0e9
dikicmila <mila.dikic97@gmail.com>
2018-03-28 18:41:28 +0200

    Kreirana JavaDoc dokumentacija za Klasu VulkanizerskaRadnja,njene atribute i metode. Generisane JavaDoc HTML strane.

--- src/gume/radnja/VulkanizerskaRadnja.java
+++ src/gume/radnja/VulkanizerskaRadnja.java
@@ -3,10 +3,24 @@ package gume.radnja;
 import java.util.LinkedList;
 import gume.AutoGuma;
 
+/**
+ * @author Mila Dikic
+ * @version 1.0
+ */
 public class VulkanizerskaRadnja {
 	
+	/**
+	 * Lista guma dostupnih u vulkanizerskoj radnji
+	 */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 
+	
+	/**
+	 * Dodaje novu gumu u ponudu vulkanizerske radnje
+	 * @param a nova guma koja se dodaje u radnju
+	 * @throws java.lang.NullPointerException ako je uneta guma null 
+	 * @throws java.lang.RuntimeException ako se u radnji vec nalazi ista takva guma
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
@@ -15,6 +29,12 @@ public class VulkanizerskaRadnja {
 		gume.addFirst(a);
 	}
 
+	
+	/**
+	 * Pronalazi gumu u vulkanizerskoj radnji
+	 * @param markaModel gume koja se trazi
+	 * @return novaLista sa trazenim gumama datog modela
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
